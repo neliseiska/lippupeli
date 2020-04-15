13eb5f6786d470d273913635d50a19e2b88c1dad
Jonne Haapalainen <jonne.haapalainen@eniram.fi>
2020-04-15 07:57:39 +0300

    Updates

---
+++ precache-manifest.c97af83cbd5fea5dd9d935eac966b4f7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "def2180dc0d14a97b310c0836abef1a9",
+    "url": "/lippupeli/index.html"
+  },
+  {
+    "revision": "8515ec6455c92c68c0b9",
+    "url": "/lippupeli/static/css/main.261a5340.chunk.css"
+  },
+  {
+    "revision": "9dad2f15ac5dbc41653c",
+    "url": "/lippupeli/static/js/2.1b4b3eec.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/lippupeli/static/js/2.1b4b3eec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8515ec6455c92c68c0b9",
+    "url": "/lippupeli/static/js/main.7bedae0d.chunk.js"
+  },
+  {
+    "revision": "76059de1f74d3375319d",
+    "url": "/lippupeli/static/js/runtime-main.8c3806bd.js"
+  }
+]);
